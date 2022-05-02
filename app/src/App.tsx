a797b7feb4485d080aa367644c27acd3f9fc46ed
Manfred Lange <manfredmlange@gmail.com>
2022-05-02 06:15:54 +0000

    Add env variable to show in frontend

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { API_ENDPOINT } from './config';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. 1802
+          Edit <code>src/App.tsx</code> and save to reload. {API_ENDPOINT}
         </p>
         <a
           className="App-link"
